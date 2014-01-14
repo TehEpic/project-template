74c2c7ec831934bd6f4edd6d1c03d5f9eae86492
EWB User <ewb@unassigned-hostname>
2014-01-14 17:58:36 -0500

    changed text

--- project.js
+++ project.js
@@ -1,12 +1,7 @@
 var sketchProc = function(processing) {
 	
-    // put variables here, just like kahn academy
-    var exampleVar = 10;
-
-    // put functions here, just like kahn academy
-    var exampleFunction = function() {
-	processing.rect(30, 20, 55, 55);
-    };
+    var h=200;
+    var w=400;
 
     // this code is executed once when the program is started
     processing.setup = function() {
@@ -14,15 +9,7 @@ var sketchProc = function(processing) {
 	// set up the size of the canvas (you probably don't want to change this!)
 	processing.size(800, 600);
 
-	// some drawing commands; feel free to change these
-	processing.fill(255, 0, 0);
 
-	// draw some text
-	processing.text("Hello, World!", exampleVar, exampleVar);
-
-	// call a function (defined up above)
-	exampleFunction();
-    };
 
     // override draw function, by default it will be called 60 times per second
     processing.draw = function() {
