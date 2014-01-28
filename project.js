659965ed068447e53095190400dbe0067f13ce86
EWB User <ewb@unassigned-hostname>
2014-01-28 17:58:21 -0500

    Made shapes

--- project.js
+++ project.js
@@ -1,16 +1,17 @@
 var sketchProc = function(processing) {
 	
-    var h=200;
-    var w=400;
+    
+
+    processing.rect (60,60,20,20);
 
     // this code is executed once when the program is started
     processing.setup = function() {
 
 	// set up the size of the canvas (you probably don't want to change this!)
 	processing.size(800, 600);
+    }
 
-
-
+ 
     // override draw function, by default it will be called 60 times per second
     processing.draw = function() {
 
